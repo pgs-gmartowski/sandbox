0ad4074a761b2969d05fdbf94d9a56637ec974f9
Grzegorz <gmartowski@pgs-soft.com>
2022-08-30 14:15:38 +0200

    01 functions

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
-function test() {
-  return 1 * 2;
+// Function types
+
+type Test = (a: number, b: number) => boolean;
+
+const MyFunction: Test = (a, b) => {
+  return a > b;
 }
 
-console.log(test());
+console.log(MyFunction(2, 4));
+
+// Funkcje wariadyczne (takie gdzie nie jest znana liczba parameterów)
+type SomeNumbers = (...params: number[]) => number
+
+const MyFunctionTwo: SomeNumbers = (...params: number[]) => {
+  return params.reduce((acc, curr) => acc + curr);
+}
+
+console.log(MyFunctionTwo(1, 2, 3, 4, 5));
