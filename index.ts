3089175dbaa2dfc27f90a51b158e6713998f2be3
Grzegorz <gmartowski@pgs-soft.com>
2022-08-30 15:03:32 +0200

    02 classes

--- index.ts
+++ index.ts
@@ -1,5 +1,57 @@
-function test() {
-  return 1 * 2;
+// Basics
+class User {
+  constructor(public name: string) {
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.name}`);
+  }
 }
 
-console.log(test());
+const user = new User('Grzegorz');
+user.sayHello();
+
+// Inheritance
+class Vehicle {
+  constructor(public numOfWheels: number) {
+  }
+}
+
+class Car extends Vehicle {
+  constructor(
+    private abs: boolean,
+    protected airbags: number,
+    public brand: string,
+    public readonly model: string,
+  ) {
+    super(4);
+  }
+
+  ride() {
+    console.log(`I have ${this.numOfWheels} wheels!`);
+  }
+}
+
+const c = new Car(true, 4, 'Volvo', 'XC90');
+console.log(c.brand);
+c.brand = 'XC60';
+console.log(c.brand);
+c.ride();
+
+// abstract
+abstract class Message {
+  constructor(private text: string) {}
+  abstract send(): void;
+  log() {
+    console.log(this.text);
+  }
+}
+
+class Email extends Message {
+  send(): void {
+    console.log()
+  }
+}
+
+const email = new Email('hello');
+email.log();
