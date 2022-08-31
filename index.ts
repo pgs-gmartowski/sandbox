300beeaeeb5156e9c2a9596613bed347b4019d42
Grzegorz <gmartowski@pgs-soft.com>
2022-08-31 11:27:04 +0200

    02 interfaces

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
-function test() {
-  return 1 * 2;
-}
+// Simplest generic
+const id = <T>(i: T) => i;
+id(7);
 
-console.log(test());
+// types
+
+type Ref<T> = {
+  current: T
+}
+const ref1: Ref<number> = {current: 1}
+const ref2: Ref<string> = {current: 'tes'}
