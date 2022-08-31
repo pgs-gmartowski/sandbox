f784f875f226a57525b4b1129f8abde7fbfb35b0
Grzegorz <gmartowski@pgs-soft.com>
2022-08-31 17:56:43 +0200

    06 build in types

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
-function test() {
-  return 1 * 2;
-}
+const t = Symbol('dsd')
+const b = Symbol('daaa')
 
-console.log(test());
+const test: Record<symbol, boolean> = {
+  [t]: true,
+  [b]: false
+}
+console.log(test);
+
+const someGeneric = {}
